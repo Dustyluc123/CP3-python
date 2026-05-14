e0884cee57f85ca29304d1c97b7cd5df224ecd4c
Dustyluc123 <lucasbrsantana@gmail.com>
2026-05-14 11:41:20 -0300

    atualizando a temperatura critica

--- cp3.py
+++ cp3.py
@@ -2,21 +2,23 @@ temperaturas = [[28, 31, 34, 33], [25, 27, 29, 28], [32, 35, 36, 34], [24, 26, 2
 sala = []
 for i in range(len(temperaturas)):
     x = 0
-    
-    for j in range(i+1, len(temperaturas)):
-        if temperaturas[i][j] >= 33:
-            x+= 1
     soma_das_temperaturas = sum(temperaturas[i])
-    media_das_temperaturas = soma_das_temperaturas / 4
+    media_das_temperaturas = soma_das_temperaturas / len(temperaturas[i])
+
+    for k in range(len(temperaturas[i])):
+        if temperaturas[i][k] >= 33:
+            x += 1
+
     sala.append(x)
-   
-   
+
     print(f"Sala {i+1}")
     print(f"Temperatura média da sala de aula {i+1}: {media_das_temperaturas}")
     print(f"Quantidade de dias com temperatura acima de 33°C na sala de aula {i+1}: {x}")
     print()
-for i in range(len(sala)):
-    if sala[i] > sala[1] and sala[i] > sala[2] and sala[i] > sala[3]:
-        print(f"A sala de aula {i+1} tem a maior quantidade de dias com temperatura acima de 33°C, com {sala[i]} dias.")
+
+maior_qtd = max(sala)
+indice_sala = sala.index(maior_qtd)
+print(f"A sala de aula {indice_sala + 1} tem a maior quantidade de dias com temperatura acima de 33°C, com {maior_qtd} dias.")
+  
     
     
